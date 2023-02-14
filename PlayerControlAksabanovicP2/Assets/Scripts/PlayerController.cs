b7628b6556821f248a69c661d54194fca7e3ce9d
AlbanianHunk <116110355+AlbanianHunk@users.noreply.github.com>
2023-02-14 09:53:27 -0800

    Worked on PlayerControl
    added PlayerControl and FollowPlayer scripts

---
+++ PlayerControlAksabanovicP2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public float speed = 20;
+    public float turnSpeed;
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
+    }
+}
