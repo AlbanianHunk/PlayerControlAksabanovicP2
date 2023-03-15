8d5eeb6358627638c860d5ada1e8c47d50f206f3
AlbanianHunk <116110355+AlbanianHunk@users.noreply.github.com>
2023-03-15 09:55:09 -0700

    Finished bonus
    Finished bonus and turning in when i get home

--- PlayerControlAksabanovicP2/Assets/Scripts/FollowPlayer.cs
+++ PlayerControlAksabanovicP2/Assets/Scripts/FollowPlayer.cs
@@ -4,16 +4,39 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
+    public GameObject follow;
+    public KeyCode Keys;
+    bool buttondown = true;
+    private Vector3 offset = new Vector3(-0, 5, -10);
+    private Vector3 offset2 = new Vector3(0, 4, -2);
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-    public GameObject player;
-    private Vector3 offset = new Vector3(0, 5, -10);
 
+    }
+
+    // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = player.transform.position + offset;
+
+        if (Input.GetKeyDown(Keys))
+        {
+            if (buttondown)
+            {
+                buttondown = false;
+            }
+            else
+            {
+                buttondown = true;
+            }
+        }
+        if (buttondown == true)
+        {
+            transform.position = follow.transform.position + offset;
+        }
+        if (buttondown == false)
+        {
+            transform.position = follow.transform.position + offset2;
+        }
     }
 }
